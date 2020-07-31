55434236861485b9bb9bc37e31f29b4b07955ba9
sehouse <sehouse@umich.edu>
2020-07-31 14:34:24 -0400

    Creation of API routes.

---
+++ routes/api-routes.js
@@ -0,0 +1,62 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({}).then(dbWorkout => {
+        dbWorkout.forEach(workout => {
+            var total = 0;
+            workout.exercises.forEach(e => {
+                total += e.time;
+            });
+            workout.totalTime = total;
+        });
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            $inc: { totalTime: req.body.time },
+            $push: { exercises: req.body }
+        },
+        { new: true }).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body).then((dbWorkout => {
+        res.json(dbWorkout);
+    })).catch(err => {
+        res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}).then(dbWorkout => {
+        console.log("ALL WORKOUTS");
+        console.log(dbWorkout);
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+router.delete("/api/workouts", ({ body }, res) => {
+    Workout.findByIdAndDelete(body.id)
+        .then(() => {
+            res.json(true);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+module.exports = router;
