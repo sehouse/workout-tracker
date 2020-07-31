dcf3e2efd95497f884a62aadab216f23d15d24bb
sehouse <sehouse@umich.edu>
2020-07-31 14:46:16 -0400

    Creation of server.js and an index.js model, so that it pulls everything together. Front end is running, but still has problems.

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+app.use(require("./routes/api-routes.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
